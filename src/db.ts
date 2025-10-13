ddc31d7e77da03072264115acad0bdecad436ace
ranaSafi <ranaa_safi@hotmail.com>
2025-10-13 17:04:49 +0200

    fixed db

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,17 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
+import pkg from "pg";
+const { Pool } = pkg;
 
+import dotenv from "dotenv";
 dotenv.config(); 
 
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432,
-  ssl: { rejectUnauthorized: false },
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "password",
+  database: process.env.DB_NAME || "curlygurl",
+  port: Number(process.env.DB_PORT) || 5432,
+});
+
+pool.on("connect", () => {
+  console.log("Connected to the database");
 });
